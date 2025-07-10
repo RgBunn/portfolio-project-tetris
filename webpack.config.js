7b2a8dff66268ab88d6197edc41d59439e976ea3
Regina <125544191+RgBunn@users.noreply.github.com>
2025-07-10 23:04:44 +0800

    final build and deploy

--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,76 @@
-const path =require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
+console.log(process.env.MODE);
 
-console.log(process.env.MODE)
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    assetModuleFilename: "assets/[name][ext]",
+    publicPath: "/",
+    path: path.resolve(__dirname, "./dist"),
+    clean: true,
+  },
 
-module.exports={
-    entry:"./src/index.js",
-    output:{
-        assetModuleFilename: 'assets/[name][ext]',
-        publicPath:'/',
-        path:path.resolve(__dirname,"./dist"),
-        clean:true
+  devtool: "source-map",
+  resolve: {
+    extensions: [".js", ".css", ".scss"],
+    alias: {
+      "@Public": path.resolve(__dirname, "public"),
     },
-
-    devtool:'source-map',
-    resolve:{
-        extensions:['.js','.css','.scss'],
-        alias:{
-        "@Public":path.resolve(__dirname,'public'),
-        }
-    },
-module: {
+  },
+  module: {
     rules: [
-        {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ['@babel/preset-env']
-                }
-                }},
-        {
-            test: /.s[a|c]ss$/,
-            use: [MiniCssExtractPlugin.loader, {
-                loader: 'css-loader',
-                options: {sourceMap: true}
-            }, {
-                loader: 'sass-loader',
-                options: {sourceMap: true}
-            }],
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
         },
-        {
-            test: /.css$/,
-            use: [MiniCssExtractPlugin.loader, {
-                loader: 'css-loader',
-                options: {sourceMap: true}
-            }],
-        },
-        {
-            test: /.(svg|png|jpg|jpeg|gif)$/,
-            type: 'asset/resource',
-        }
-    ]
-},
-    plugins:[new HtmlWebpackPlugin(
-        {
-            template:path.resolve(__dirname,"./public/index.html"),
-            inject:'body',
-            title:'Webpack App'
-        }
-    )
-        ]
-}
+      },
+      {
+        test: /.s[a|c]ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+          {
+            loader: "sass-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: { sourceMap: true },
+          },
+        ],
+      },
+      {
+        test: /.(svg|png|jpg|jpeg|gif)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "./public/index.html"),
+      inject: "body",
+      title: "Webpack App",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "public/images", to: "images" }],
+    }),
+  ],
+};
